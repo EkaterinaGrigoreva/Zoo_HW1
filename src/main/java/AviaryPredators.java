6179ac324dd348e42939f3168e4fbe848c27f394
EkaterinaGrigoreva <39069240+EkaterinaGrigoreva@users.noreply.github.com>
2019-09-09 08:47:18 +0400

    животные едят все вольеры не работают

---
+++ src/main/java/AviaryPredators.java
@@ -0,0 +1,25 @@
+public class AviaryPredators {
+int cage = 3;
+int count = 0;
+    public void add() {
+        if (count >= cage) {
+            System.out.println("мест нет");
+        } else {
+            System.out.println("добро пожаловать домой");
+        }
+
+    }
+
+    public static void addAnimal(Predators animalP) {
+
+
+        Predators[] predator = new Predators[3];
+        for (int i = 0; i < predator.length; i++) {
+            predator[i] = new Predators(animalP[i]){};
+
+            animalP.add(animalP[i]);
+        }
+
+
+}
+}
